ef23405e4992b3d21f1b17e5f92adeb402af67fe
Felix H W <fehawen@protonmail.com>
2025-02-26 15:53:12 +0100

    Update

--- install.sh
+++ install.sh
@@ -10,6 +10,9 @@ BACKUP="$DEST.backup.$(date +%s)"
 if [ -d "$DEST" ] && [ ! -h "$DEST" ]; then
     printf 'Creating backup: %s\n' "$BACKUP"
     mv "$DEST" "$BACKUP"
+else
+    printf 'Creating directory: %s\n' "$DEST"
+    mkdir -p "$DEST"
 fi
 
 printf 'Symlinking config: %s -> %s\n' "$PWD/nvim" "$DEST"
