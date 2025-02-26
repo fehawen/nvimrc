84aba5e22046a55b1fd18d5306fa6023167a6877
Felix H W <fehawen@protonmail.com>
2025-02-26 16:01:41 +0100

    Fix

--- install.sh
+++ install.sh
@@ -4,15 +4,18 @@
 # install.sh
 #
 
-DEST="$HOME/.config/nvim"
+CONF="$HOME/.config"
+DEST="$CONF/nvim"
 BACKUP="$DEST.backup.$(date +%s)"
+
+if [ ! -d "$CONF" ]; then
+    printf 'Creating directory: %s\n' "$CONF"
+    mkdir -p "$CONF"
+fi
 
 if [ -d "$DEST" ] && [ ! -h "$DEST" ]; then
     printf 'Creating backup: %s\n' "$BACKUP"
     mv "$DEST" "$BACKUP"
-else
-    printf 'Creating directory: %s\n' "$DEST"
-    mkdir -p "$DEST"
 fi
 
 printf 'Symlinking config: %s -> %s\n' "$PWD/nvim" "$DEST"
